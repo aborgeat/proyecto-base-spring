6d21a153823ce69ed5ae86ee3c134d71c6e2eadd
aborgeat <andresborgeat@hotmail.com>
2016-10-19 00:58:40 -0300

    travis 7

---
+++ src/test/java/ar/edu/grupoesfera/cursospring/modelo/TestModelo.java
@@ -0,0 +1,13 @@
+package ar.edu.grupoesfera.cursospring.modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestModelo {
+
+	@Test
+	public void testOk (){
+		Assert.assertTrue(1==1);
+		
+	}
+}
